7ff39f4b6013fc45962779526ef445f5b0050dd0
Oskar Sęk <o.sek@kaft.pl>
2022-09-23 13:59:44 +0200

    exercise 21.5 heroku

---
+++ src/main/java/com/crud/tasks/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.crud.tasks.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Value("${application.frontend.url}")
+    private String frontUrl;
+
+    @Bean
+    WebMvcConfigurer corsConfiguration() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontUrl)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
