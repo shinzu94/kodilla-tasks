6c6b4a7ec6900780f4033d09e9529caf0b3c8f54
Oskar Sęk <o.sek@kaft.pl>
2022-09-28 20:44:16 +0200

    exercise 23.4 email 2

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,25 +1,43 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.Mail;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
-import com.crud.tasks.trello.client.TrelloClient;
+import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.service.TrelloService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("v1/trello")
 @RequiredArgsConstructor
 @Slf4j
 public class TrelloController {
-    private final TrelloClient trelloClient;
+    private final TrelloService trelloService;
+    private final MailProperties mailProperties;
+    private final SimpleEmailService simpleEmailService;
+
+    @GetMapping("test")
+    public String getTest() {
+        simpleEmailService.send(Mail.builder()
+                .receiverEmail("oskar.sek94@gmail.com")
+                .subject("Test")
+                .message("test")
+                .toCc(Optional.of("test"))
+                .build());
+
+        return mailProperties.getUsername() + " - " + mailProperties.getPassword();
+    }
 
     @GetMapping("boards")
     public List<TrelloBoardDto> getTrelloBoards() {
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+        List<TrelloBoardDto> trelloBoards = trelloService.fetchTrelloBoards();
 
         trelloBoards
                 .forEach(trelloBoardDto -> {
@@ -35,6 +53,6 @@ public class TrelloController {
 
     @PostMapping("cards")
     public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
-        return trelloClient.createNewCard(trelloCardDto);
+        return trelloService.createTrelloCard(trelloCardDto);
     }
 }
