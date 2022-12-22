e20ff5ffd028f83e96b139300280a41c53eee3b2
Oskar Sęk <o.sek@kaft.pl>
2022-12-22 20:46:38 +0100

    fix constructor

--- src/main/java/com/crud/tasks/domain/TrelloListDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloListDto.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloListDto {
+
     private String id;
 
     private String name;
